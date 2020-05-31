6c1c04d6d99aeee3ca298f90cdada968ccdbbd7e
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-05-31 13:48:43 +0300

    Getting started on Context

---
+++ NorthwindBackend/DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\ProjectsV13;Database=Northwind;Integrated security=true;");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
